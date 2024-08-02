f600136953f3237bdb93748f6ebe90c8c8ececec
kauan junker <kauanjunker@gmail.com>
2024-08-02 12:18:00 -0300

    fetch product featured

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,273 +1,76 @@
+import { wixClientServer } from "@/lib/wixClientServer";
+import { products } from "@wix/stores";
 import Image from "next/image";
 import Link from "next/link";
+import DOMPurify from "isomorphic-dompurify";
 
-const ProductList = () => {
+const PRODUCT_PER_PAGE = 20;
+
+const ProductList = async ({
+  categoryId,
+  limit,
+}: {
+  categoryId: string;
+  limit?: number;
+}) => {
+  const wixClient = await wixClientServer();
+  const res = await wixClient.products
+    .queryProducts()
+    .eq("collectionIds", categoryId)
+    .limit(limit || PRODUCT_PER_PAGE)
+    .find();
+
   return (
     <div className="mt-12 flex gap-x-6 gap-y-16 justify-between flex-wrap">
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="/cr7.jpg"
-            alt=""
-            sizes="25vw"
-            fill
-            className="absolute rounded-md object-cover z-10 hover:opacity-0 
-            transition-opacity ease-out duration-500"
-          />
-          <Image
-            src="/cristiano.jpg"
-            alt=""
-            sizes="25vw"
-            fill
-            className="absolute rounded-md object-cover"
-          />
-        </div>
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500 ">My description</div>
-        <button
-          className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 
-        text-xs hover:bg-lama hover:text-white"
+      {res.items.map((product: products.Product) => (
+        <Link
+          href={`/${product.slug}`}
+          className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+          key={product._id}
         >
-          Add to Cart
-        </button>
-      </Link>
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="/cr7.jpg"
-            alt=""
-            sizes="25vw"
-            fill
-            className="absolute rounded-md object-cover z-10 hover:opacity-0 
+          <div className="relative w-full h-80">
+            <Image
+              src={product.media?.mainMedia?.image?.url || "/product.png"}
+              alt=""
+              sizes="25vw"
+              fill
+              className="absolute rounded-md object-cover z-10 hover:opacity-0 
             transition-opacity ease-out duration-500"
-          />
-          <Image
-            src="/cristiano.jpg"
-            alt=""
-            sizes="25vw"
-            fill
-            className="absolute rounded-md object-cover"
-          />
-        </div>
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500 ">My description</div>
-        <button
-          className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 
+            />
+            {product.media?.items && (
+              <Image
+                src={product.media?.items[1]?.image?.url || "/product.png"}
+                alt=""
+                sizes="25vw"
+                fill
+                className="absolute rounded-md object-cover"
+              />
+            )}
+          </div>
+          <div className="flex justify-between">
+            <span className="font-medium">{product.name}</span>
+            <span className="font-semibold">${product.price?.price}</span>
+          </div>
+          {product.additionalInfoSections && (
+            <div
+              className="text-sm text-gray-500"
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(
+                  product.additionalInfoSections.find(
+                    (section: any) => section.title === "shortDesc"
+                  )?.description || ""
+                ),
+              }}
+            ></div>
+          )}
+          <button
+            className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 
         text-xs hover:bg-lama hover:text-white"
-        >
-          Add to Cart
-        </button>
-      </Link>
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="/cr7.jpg"
-            alt=""
-            sizes="25vw"
-            fill
-            className="absolute rounded-md object-cover z-10 hover:opacity-0 
-            transition-opacity ease-out duration-500"
-          />
-          <Image
-            src="/cristiano.jpg"
-            alt=""
-            sizes="25vw"
-            fill
-            className="absolute rounded-md object-cover"
-          />
-        </div>
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500 ">My description</div>
-        <button
-          className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 
-        text-xs hover:bg-lama hover:text-white"
-        >
-          Add to Cart
-        </button>
-      </Link>
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="/cr7.jpg"
-            alt=""
-            sizes="25vw"
-            fill
-            className="absolute rounded-md object-cover z-10 hover:opacity-0 
-            transition-opacity ease-out duration-500"
-          />
-          <Image
-            src="/cristiano.jpg"
-            alt=""
-            sizes="25vw"
-            fill
-            className="absolute rounded-md object-cover"
-          />
-        </div>
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500 ">My description</div>
-        <button
-          className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 
-        text-xs hover:bg-lama hover:text-white"
-        >
-          Add to Cart
-        </button>
-      </Link>
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="/cr7.jpg"
-            alt=""
-            sizes="25vw"
-            fill
-            className="absolute rounded-md object-cover z-10 hover:opacity-0 
-            transition-opacity ease-out duration-500"
-          />
-          <Image
-            src="/cristiano.jpg"
-            alt=""
-            sizes="25vw"
-            fill
-            className="absolute rounded-md object-cover"
-          />
-        </div>
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500 ">My description</div>
-        <button
-          className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 
-        text-xs hover:bg-lama hover:text-white"
-        >
-          Add to Cart
-        </button>
-      </Link>
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="/cr7.jpg"
-            alt=""
-            sizes="25vw"
-            fill
-            className="absolute rounded-md object-cover z-10 hover:opacity-0 
-            transition-opacity ease-out duration-500"
-          />
-          <Image
-            src="/cristiano.jpg"
-            alt=""
-            sizes="25vw"
-            fill
-            className="absolute rounded-md object-cover"
-          />
-        </div>
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500 ">My description</div>
-        <button
-          className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 
-        text-xs hover:bg-lama hover:text-white"
-        >
-          Add to Cart
-        </button>
-      </Link>
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="/cr7.jpg"
-            alt=""
-            sizes="25vw"
-            fill
-            className="absolute rounded-md object-cover z-10 hover:opacity-0 
-            transition-opacity ease-out duration-500"
-          />
-          <Image
-            src="/cristiano.jpg"
-            alt=""
-            sizes="25vw"
-            fill
-            className="absolute rounded-md object-cover"
-          />
-        </div>
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500 ">My description</div>
-        <button
-          className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 
-        text-xs hover:bg-lama hover:text-white"
-        >
-          Add to Cart
-        </button>
-      </Link>
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="/cr7.jpg"
-            alt=""
-            sizes="25vw"
-            fill
-            className="absolute rounded-md object-cover z-10 hover:opacity-0 
-            transition-opacity ease-out duration-500"
-          />
-          <Image
-            src="/cristiano.jpg"
-            alt=""
-            sizes="25vw"
-            fill
-            className="absolute rounded-md object-cover"
-          />
-        </div>
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500 ">My description</div>
-        <button
-          className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 
-        text-xs hover:bg-lama hover:text-white"
-        >
-          Add to Cart
-        </button>
-      </Link>
+          >
+            Add to Cart
+          </button>
+        </Link>
+      ))}
     </div>
   );
 };
