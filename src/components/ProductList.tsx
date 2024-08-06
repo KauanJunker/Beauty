37777361ab2db2309e01ea4616a3eca307eda7ce
kauan junker <kauanjunker@gmail.com>
2024-08-06 09:51:12 -0300

    pagination component

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -3,8 +3,9 @@ import { products } from "@wix/stores";
 import Image from "next/image";
 import Link from "next/link";
 import DOMPurify from "isomorphic-dompurify";
+import Pagination from "./Pagination";
 
-const PRODUCT_PER_PAGE = 20;
+const PRODUCT_PER_PAGE = 8;
 
 const ProductList = async ({
   categoryId,
@@ -16,15 +17,44 @@ const ProductList = async ({
   searchParams?: any;
 }) => {
   const wixClient = await wixClientServer();
-  const res = await wixClient.products
+
+  const productQuery = wixClient.products
     .queryProducts()
+    .startsWith("name", searchParams?.name || "")
     .eq("collectionIds", categoryId)
+    .hasSome(
+      "productType",
+      searchParams?.type ? [searchParams.type] : ["physical", "digital"]
+    )
+    .gt("priceData.price", searchParams?.min || 0)
+    .lt("priceData.price", searchParams?.max || 999999)
     .limit(limit || PRODUCT_PER_PAGE)
-    .find();
+    .skip(
+      searchParams?.page
+        ? parseInt(searchParams?.page) * (limit || PRODUCT_PER_PAGE)
+        : 0
+    );
+
+  // .find();
+
+  if (searchParams?.sort) {
+    console.log("entrou");
+    const [sortType, sortBy] = searchParams.sort.split(" ");
+    console.log(`Sorting by ${sortBy} in ${sortType} order`);
+
+    if (sortType === "asc") {
+      productQuery.ascending(sortBy);
+    }
+    if (sortType === "desc") {
+      productQuery.descending(sortBy);
+    }
+  }
+
+  const res = await productQuery.find();
 
   return (
     <div className="mt-12 flex gap-x-6 gap-y-16 justify-between flex-wrap">
-      {res.items.map((product: products.Product) => (
+      {res?.items.map((product: products.Product) => (
         <Link
           href={`/${product.slug}`}
           className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
@@ -73,6 +103,11 @@ const ProductList = async ({
           </button>
         </Link>
       ))}
+      <Pagination
+        currentPage={res.currentPage || 0}
+        hasPrev={res.hasPrev()}
+        hasNext={res.hasNext()}
+      />
     </div>
   );
 };
